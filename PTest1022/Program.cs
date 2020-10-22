ce4163ab4490b77177fcbf38800c6f4ac8eddf95
DaisukeKameyama <31030@ojs.ac.jp>
2020-10-22 10:12:44 +0900

    試験1022

--- PTest1022/Program.cs
+++ PTest1022/Program.cs
@@ -21,7 +21,7 @@ namespace PTest1022 {
             //６以上…「６以上です」
 
             /*** ●●● ここへ解答を入力 ●●● ***/
-            
+            outStr = inputNum <= 5 ? "５以下です" : "６以上です"; 
 
 
 
@@ -39,7 +39,7 @@ namespace PTest1022 {
             //※動作チェックする場合は「Exam_2_Stab()を使用すること」
 
             /*** ●●● ここへ解答を入力 ●●● ***/
-
+            data = Exam_2_Stab() ?? -9999;
 
 
 
@@ -52,8 +52,8 @@ namespace PTest1022 {
         //問題２テスト用スタブ
         private int? Exam_2_Stab() {
             //▼▼▼▼▼　以下のコメントを切り替えて確認をする 
-            //return null;
-            return 150;
+            return null;
+            //return 150;
             //▲▲▲▲▲
         }
 
@@ -62,14 +62,14 @@ namespace PTest1022 {
             string title = "";
 
             //▼▼▼▼▼　以下のコメントを切り替えて確認をする 
-            Book book = new Book() { Title = "銀河鉄道の夜", Price = 411, Pages = 276 };
-            //Book book = new Book();
+            //Book book = new Book() { Title = "銀河鉄道の夜", Price = 411, Pages = 276 };
+            Book book = new Book();
             //▲▲▲▲▲
 
             //bookオブジェクトのタイトル名を出力せよ
             //bookオブジェクトがnullの場合はそのままnullを返却すること
             /*** ●●● ここへ解答を入力 ●●● ***/
-
+            title = book?.Title;
 
 
 
@@ -83,19 +83,17 @@ namespace PTest1022 {
         private void Exam_4() {
             Console.WriteLine($"\n〇問題４");
             var books = new Books().GetBooks();
-            int pageSum = 0;
+            //int pageSum = 0;
 
             //金額が1200円以上の書籍を抽出せよ
             //出力については「タイトル」と「金額」を出力すること
-            
+
             /*** ●●● ここへ解答を入力 ●●● ***/
-
-
-
-
-
+            foreach(var book in books.Where(x => x.Price >= 1200)) {
+                Console.WriteLine($"{book.Title}:{book.Price}");
+            }
             /*** ●●●●● ここまで ●●●●● ***/
-            Console.WriteLine($"ページの合計は{ pageSum }ページです。");
+            //Console.WriteLine($"ページの合計は{ pageSum }ページです。");
         }
 
         //問題５
@@ -107,10 +105,9 @@ namespace PTest1022 {
             //出力については「タイトル」と「ページ数」を出力すること
 
             /*** ●●● ここへ解答を入力 ●●● ***/
-
-
-
-
+            foreach(var book in books.Where(x => x.Title.Contains("物語") && x.Pages >= 400)) {
+                Console.WriteLine($"{book.Title}:{book.Pages}");
+            }
 
             /*** ●●●●● ここまで ●●●●● ***/
         }
@@ -123,11 +120,9 @@ namespace PTest1022 {
             //出力については改行をせず、ヨコにスペースを空けて出力する
             // 出力例）79 65 53 45 35 34 20 12
             /*** ●●● ここへ解答を入力 ●●● ***/
-
-
-
-
-
+            foreach(var num in numbers.OrderByDescending(n => n).Distinct()) {
+                Console.Write(num+" ");
+            }
             /*** ●●●●● ここまで ●●●●● ***/
         }
 
@@ -138,7 +133,7 @@ namespace PTest1022 {
 
             //引数で受け取った numbers の平均を変数 avg に求めよ
             /*** ●●● ここへ解答を入力 ●●● ***/
-
+            avg = numbers.Average();
 
 
 
@@ -158,12 +153,10 @@ namespace PTest1022 {
             // 出力例）60 36 135 195 102 105 195 159 237
 
             /*** ●●● ここへ解答を入力 ●●● ***/
-
-
-
-
-
-
+            foreach(var num in numbers.Select(n => n * 3)) {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
             /*** ●●●●● ここまで ●●●●● ***/            
         }
 
